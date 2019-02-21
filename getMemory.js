04afe4d4bcec1d17032d37c0498b78a8eaa862e6
Timothy Janssen <timothy.janssen@sap.com>
2019-02-21 14:48:27 -0800

    c

--- getMemory.js
+++ getMemory.js
@@ -1,8 +1,31 @@
+var options = {
+    uri:    "https://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/",
+    method:  "GET",
+    json:    true,
+    transform2xxOnly: true,
+    transform: function(body, response, resolveWithFullResponse) {
+  		return {'token': response.headers['x-csrf-token'], 'cookie': response.headers["set-cookie"], 'data': body};
+	},
+    headers: {       
+         "X-CSRF-Token":  "Fetch",
+         "Authorization": "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==", // base64 encoding of administration01:Welcome5
+         "Content-Type":  "application/json"
+    }
+};
+
+getToken = function()  {
+    return new Promise((resolve, reject) => {
+        request(options)
+        .then( data => { resolve(data); } )
+        .catch( data => { reject(data); } );
+    });
+}	
+
 Window.webchatMethods = {
 	getMemory: function () {
 
 
-		return Window.csrf_token || csrf.getToken()
+		return Window.csrf_token || getToken()
 		.then( function(token_data){
 			return {
 				"memory": {
@@ -15,9 +38,9 @@ Window.webchatMethods = {
 	}
 }
 
-document.getElementById("token-div").onload = function () {
+document.getElementById("token-div").onload() = function () {
 
-	Window.csrf_token = csrf.getToken()
+	Window.csrf_token = getToken()
 						.then( function(token_data){
 							return {
 								"memory": {
@@ -30,3 +53,4 @@ document.getElementById("token-div").onload = function () {
 
 	document.getElementById("token-div").innerHTML = Window.csrf_token + "";
 }
+

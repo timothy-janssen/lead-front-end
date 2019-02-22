95b0b6fd76cc1ff03dec5b5fcbb75208e4aa3973
Timothy Janssen <timothy.janssen@sap.com>
2019-02-22 13:44:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 //var request = require('request-promise');
 
 var options = {
-    method:  "GET",
-    crossdomain: true,
-    mode: 'no-cors',
     responseType: 'json',
     transformResponse: function(data) {
   		return {'token': data.response.headers['x-csrf-token'], 'cookie': data.response.headers["set-cookie"], 'data': data.body};
 	},
     headers: {       
-        "X-CSRF-Token":  "Fetch",
-        "Authorization": "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==", // base64 encoding of administration01:Welcome5
-    	"Access-Control-Allow-Origin": "https://lead-bot-front-end.herokuapp.com",
-    	"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
-    	"Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Content-Type":  "application/json"
+        
+        : , // base64 encoding of administration01:Welcome5
+        :  
     }
 };
 
 const getToken = () => {
     return d3.request("https://cors-anywhere.herokuapp.com/" + "http://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/")
     	.mimeType("application/json")
+    	.header("X-CSRF-Token",   "Fetch")
+    	.header("Authorization",  "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==")
+    	.header("Content-Type",   "application/json")
     	.response(function(xhr) { return JSON.parse(xhr.responseText); })
     	.get(options, function (token_data) { 
     			Window.csrf_token = { 	
@@ -31,7 +28,7 @@ const getToken = () => {
 					"merge": true
 				}; 
 				return Window.csrf_token;
-			});
+		});
 }	
 
 window.webchatMethods = {

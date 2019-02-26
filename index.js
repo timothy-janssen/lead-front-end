39adcb5320eea55ad41977e0860bebc8217db67f
Timothy Janssen <timothy.janssen@sap.com>
2019-02-26 09:47:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ var options = {
 };
 
 const getToken = () => {
-    return d3.request("https://cors-anywhere.herokuapp.com/" + "http://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/")
+    return d3.request("https://radiant-sea-97792.herokuapp.com/" + "http://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/")
     	.mimeType("application/json")
     	.header("X-CSRF-Token",   "Fetch")
     	.header("Authorization",  "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==")
     	.header("Content-Type",   "application/json")
-    	.response(function(xhr) { return JSON.parse(xhr.responseText); })
+ //   	.response(function(xhr) { return JSON.parse(xhr.responseText); })
     	.get( function (error, token_data, a, b, c) { 
+    		var header = token_data.getResponseHeader('x-csrf-token');
     			Window.csrf_token = { 	
     		    	"memory": {
 						"cookie": token_data.cookie,

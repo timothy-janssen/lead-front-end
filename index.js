37075797c2b2e917ad7ef06c0730309d5cf606f9
Timothy Janssen <timothy.janssen@sap.com>
2019-02-28 11:04:56 -0800

    z

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 //var request = require('request-promise');
 
-var options = {
-    transformResponse: function(data) {
-  		return { 'token': data.response.headers['x-csrf-token'], 
-  				 'cookie': data.response.headers["set-cookie"], 
-  				 'data': data.body
-  		};
-    }
-};
-
 const getToken = () => {
     return d3.request("https://radiant-sea-97792.herokuapp.com/" + "http://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/")
     	.mimeType("application/json")
     	.header("X-CSRF-Token",   "Fetch")
     	.header("Authorization",  "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==")
     	.header("Content-Type",   "application/json")
-    	.header("x-access-control-allow-credentials", "true")
-    	.header("Access-Control-Allow-Origins", "http://my341721.crm.ondemand.com")
     	.header("x-access-control-allow-credentials", "true")
  //   	.response(function(xhr) { return JSON.parse(xhr.responseText); })
     	.get( function (error, token_data) { 
@@ -41,9 +30,3 @@ window.webchatMethods = {
 		return Window.csrf_token || getToken();
 	}
 }
-
-
-function getTokenAsync () {
-	getToken()	;
-}
-
